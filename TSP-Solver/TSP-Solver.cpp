1675432085631bcec93cd04ad85d7afb33220273
GabrielBall05 <gabrielb4556@gmail.com>
2026-06-18 12:44:50 -0400

    Bound Computation Implemented

--- TSP-Solver/TSP-Solver.cpp
+++ TSP-Solver/TSP-Solver.cpp
@@ -71,8 +71,43 @@ vector<vector<double>> generateAdjacencyMatrix(const vector<City>& cities) {
 }
 
 //Returns the bound for the given node
-double computeBound(const vector<vector<double>>& adjMat, int curCity, const vector<City>& unvisitedCities) {
+double computeBound(const vector<vector<double>>& adjMat, int curCity, const vector<int>& unvisitedCities, double costSoFar) {
+    double estimatedCost = costSoFar; //Initialize to the cost so far
 
+    double minEdge = numeric_limits<double>::infinity();
+    if (unvisitedCities.size() == 0) { //If all cities are visited, go back to city 0
+        minEdge = adjMat[curCity][0];
+    }
+    else {
+        //Find the minimum outgoing edge from curCity to the unvisited cities
+        for (int i = 0; i < unvisitedCities.size(); i++) {
+            double distance = adjMat[curCity][unvisitedCities[i]];
+            if (distance < minEdge) minEdge = distance;
+        }
+    }
+    //Add curCity's minimum outgoing edge to bound
+    estimatedCost += minEdge;
+
+    //For every unvisited city, add its minimum edge to other unvisited cities to the estimated cost
+    for (int cityId : unvisitedCities) {
+        //Find minimum outgoing edge from this unvisited city to the other unvisited cities
+        double minEdge = numeric_limits<double>::infinity();
+        for (int i = 0; i < unvisitedCities.size(); i++) {
+            //If source == destination, skip
+            if (cityId == unvisitedCities[i]) continue;
+
+            double distance = adjMat[cityId][unvisitedCities[i]];
+            if (distance < minEdge) minEdge = distance;
+        }
+        //Also check distance from this unvisited city to the starting city (0)
+        if (adjMat[cityId][0] < minEdge) minEdge = adjMat[cityId][0];
+
+        //Add this unvisited city's minimum outgoing cost to the total estimated cost (bound)
+        estimatedCost += minEdge;
+    }
+
+    //Return the final lower bound for this node
+    return estimatedCost;
 }
 
 //TSP Solver Main Algorithm
@@ -81,7 +116,16 @@ void solveTSP(const vector<vector<double>>& adjMat) {
 }
 
 int main() {
-    
+    //Get n from user
+    cout << "Number of vertices/cities:";
+    int n;
+    cin >> n;
+
+    //Generate graph and adjacency matrix
+    vector<vector<double>> adjMat = generateAdjacencyMatrix(generateCities(n));
+
+    //Send adjacency matrix to the solver function
+    solveTSP(adjMat);
 
 
 }
